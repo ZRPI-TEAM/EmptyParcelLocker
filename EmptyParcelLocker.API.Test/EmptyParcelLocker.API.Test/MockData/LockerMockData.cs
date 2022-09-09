7d849f45cc533907281e9cb917f986e24c13fa47
Jan Urbaś <janurbas39@gmail.com>
2022-09-09 10:27:54 +0200

    Add EmptyParcelLocker.API.Test project and implement 3 basic controllers tests.

---
+++ EmptyParcelLocker.API.Test/EmptyParcelLocker.API.Test/MockData/LockerMockData.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.AccessControl;
+using EmptyParcelLocker.API.Data.Models;
+
+namespace EmptyParcelLocker.API.Test.MockData;
+
+public class LockerMockData
+{
+    private static readonly Random _random = new Random();
+    
+    public static List<Locker> GetLockers(int quantity)
+    {
+        var lockerTypes = LockerTypeMockData.GetLockerTypes();
+        var lockers = new List<Locker>();
+
+        for (var i = 0; i <= quantity; i++)
+        {
+            var randomLockerType = lockerTypes[_random.Next(0, 3)];
+            var locker = new Locker
+            {
+                Id = Guid.NewGuid(),
+                IsEmpty = _random.Next(0, 100) % 2 == 0,
+                LockerType = randomLockerType,
+                LockerTypeId = randomLockerType.Id,
+            };
+            
+            lockers.Add(locker);
+        }
+
+        return lockers;
+    }
+}
---
+++ EmptyParcelLocker.API.Test/EmptyParcelLocker.API.Test/MockData/ParcelLockerMockData.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+using EmptyParcelLocker.API.Data.Models;
+
+namespace EmptyParcelLocker.API.Test.MockData;
+
+public class ParcelLockerMockData
+{
+    private static readonly Random _random = new();
+    
+    public static List<ParcelLocker> GetParcelLockers(int quantity)
+    {
+        var parcelLockers = new List<ParcelLocker>();
+        
+        for (var i = 0; i < quantity; i++)
+        {
+            var lockers = LockerMockData.GetLockers(_random.Next(3, 13));
+            
+            var parcelLocker = new ParcelLocker
+            {
+                Id = Guid.NewGuid(),
+                Name = $"ParcelLocker{i}",
+                Address = $"street{i};houseNumber{i};ApartmentNumber{i};{i}{i}-{i}{i}{i};City{i}",
+            };
+            
+            foreach (var locker in lockers)
+            {
+                locker.ParcelLocerId = parcelLocker.Id;
+            }
+
+            parcelLocker.Lockers = lockers;
+            parcelLockers.Add(parcelLocker);
+        }
+
+        return parcelLockers;
+    }
+}
